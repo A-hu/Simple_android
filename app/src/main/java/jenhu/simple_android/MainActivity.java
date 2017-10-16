085c817a273d2ba2efae0817cd3f9007572b0c30
A-hu <louis0911317399@gmail.com>
2017-10-16 14:09:01 +0800

    Click Event

--- app/src/main/java/jenhu/simple_android/MainActivity.java
+++ app/src/main/java/jenhu/simple_android/MainActivity.java
@@ -2,12 +2,39 @@ package jenhu.simple_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etName, etPasswrd;
+    private TextView tvResult;
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName = (EditText) findViewById(R.id.etName);
+        etPasswrd = (EditText) findViewById(R.id.etPassword);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                String password = etPasswrd.getText().toString();
+                String text = "Name : " + name + "\nPassword :" + password;
+                tvResult.setText(text);
+            }
+        });
     }
+
+//    public void onLoginClick(View view) {
+//        String name = etName.getText().toString();
+//        String password = etPasswrd.getText().toString();
+//        String text = "Name : " + name + "\nPassword :" + password;
+//        tvResult.setText(text);
+//    }
 }
