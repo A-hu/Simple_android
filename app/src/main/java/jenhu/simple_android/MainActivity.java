655fbd867eec34f8e42e348331f97331d2da65bf
A-hu <louis0911317399@gmail.com>
2017-10-16 16:15:36 +0800

    Touch Event

--- app/src/main/java/jenhu/simple_android/MainActivity.java
+++ app/src/main/java/jenhu/simple_android/MainActivity.java
@@ -2,15 +2,19 @@ package jenhu.simple_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etName, etPasswrd;
     private TextView tvResult;
     private Button btLogin;
+    private TextView tvMessage;
+    private int pointCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,47 @@ public class MainActivity extends AppCompatActivity {
                 String text = "Name : " + name + "\nPassword :" + password;
                 tvResult.setText(text);
             }
+        });
+
+        findViews();
+    }
+
+    private void findViews(){
+        tvMessage = (TextView) findViewById(R.id.tvMessage);
+        RelativeLayout relativelayout = (RelativeLayout) findViewById(R.id.relativelayout);
+
+        relativelayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("first pointer: (%.1f, %.1f), %n",
+                        event.getX(), event.getY()));
+                sb.append("touch state: ");
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        sb.append("ACTION_DOWN\n");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        sb.append("ACTION_MOVE\n");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        sb.append("ACTION_UP\n");
+                        break;
+                    default:
+                        sb.append("\n");
+                        break;
+                }
+
+                int pointCount = event.getPointerCount();
+                sb.append(String.format("point count: %d %n", pointCount));
+                for(int i = 0; i < pointCount; i++){
+                    sb.append(String.format("pointer: %d: (%.1f, %.1f) %n",
+                            event.getPointerId(i), event.getX(i), event.getY(i)));
+                }
+                tvMessage.setText(sb);
+                return true;
+            }
+
         });
     }
 
